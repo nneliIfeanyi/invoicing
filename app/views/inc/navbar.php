a59a5de401b2a42fa64e5a939130839a41d7247a
Nneli Ifeanyi <Nneli Ifeanyi>
2024-04-25 20:56:26 +0100

    yeah yeah

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,14 +1,14 @@
 
 <nav class="navbar navbar-expand-lg p-2 navbar-dark bg-dark mb-3 sticky-top border-bottom border-primary">
   <div class="container">
-    <a class="navbar-brand" href="<?= URLROOT;?>/pages">Invoice<span class="text-primary">Online</span></a>
+    <a class="navbar-brand" href="<?= URLROOT;?>/pages/index">Invoice<span class="text-primary">Online</span></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
+          <a class="nav-link" href="<?php echo URLROOT;?>pages/index">Home</a>
         </li>
         <!-- <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about"><i class="fa fa-plus" aria-hidden="true"></i> About</a>
--- public/offline.php
+++ public/offline.php
@@ -6,7 +6,8 @@
   <title>invoice Online</title>
 </head>
 <body>
-<h1 style="text-align: center;color: #198754;">You are currently offline</h1>
+  <h1 style="text-align: center;color: #198754;">You are currently offline</h1>
+  <button onclick="history.back()" style="text-align:center; border: none;padding: 12px 10px;">Go Back</button>
 </body>
 </html>
 
