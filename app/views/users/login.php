2a829b1ff8a7cd7f3a7cbab33ec431f120f9a5a1
Nneli Ifeanyi <Nneli Ifeanyi>
2024-04-25 23:28:17 +0100

    dynamicCache

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <div class="row">
     <div class="col-md-6 mx-auto">
-      <div class="card card-body bg-light mb-5">
+      <div class="card card-body bg-light my-5">
        <h1 class="h2 m-0">Simple And Easy To Use Online Invoicing Software For Business Owners.</h1>
       <p class="text-primary fw-semibold">Login to continue</p>
         <p>Please fill in your credentials to login.</p>
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,24 @@
 const staticCacheName = 'site-static';
 const dynamicCache = 'site-dynamic';
 const assets = [
+   'site.webmanifest',
    'index.php', 
    'offline.php',
    'app.js',
    'css/bootstrap.css',
    'css/font-awesome.css',
-   'css/style.css',
-   'css/styles.css',
    'js/bootstrap.bundle.min.js',
+   'js/jquery.js',
+   'js/parsley.min.js',
+   'webfonts/fa-brands-400.ttf',
+   'webfonts/fa-brands-400.woff2',
+   'webfonts/fa-regular-400.ttf',
+   'webfonts/fa-regular-400.woff2',
+   'webfonts/fa-solid-900.ttf',
+   'webfonts/fa-solid-900.woff2',
+   'webfonts/fa-v4compatibility.ttf',
+   'webfonts/fa-v4compatibility.woff2',
+   'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap',
 ];
 
 const limitCache = (name, size) => {
@@ -50,7 +60,7 @@ self.addEventListener("fetch", event => {
          return cacheRes || fetch(event.request).then(fetchRes => {
             return caches.open(dynamicCache).then(cache => {
                cache.put(event.request.url, fetchRes.clone());
-               limitCache(dynamicCache, 20);
+               limitCache(dynamicCache, 1);
                return fetchRes;
             })
          });
