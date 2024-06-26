895da3a0873ec86d621767110a859260cd6ce5f8
Stanvic <stanvicbest@gmail.com>
2024-06-26 05:47:41 -0700

    pos3

--- app/views/posts/print_pos.php
+++ app/views/posts/print_pos.php
@@ -10,15 +10,17 @@ $obj1->align =1;
 $obj1->format = 3;
 array_push($a,$obj1);
 
+
+	
+$obj5->type = 4;
+$obj5->content = "<center><span style=\"font-weight:bold; font-size:20px;\">This is sample text</span></center>";
+array_push($a,$obj5);
+
 $obj6->type = 0;
 $obj6->content = ' ';
 $obj6->bold = 0;
 $obj6->align = 0;
 array_push($a,$obj6);
-	
-$obj5->type = 4;
-$obj5->content = "<center><span style=\"font-weight:bold; font-size:20px;\">This is sample text</span></center>";
-array_push($a,$obj5);
 
 $obj7->type = 0;
 $obj7->content = 'This text has<br />two lines';
