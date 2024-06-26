a2a0e3dcfa499ccc4645a6eecbec93ca1578b434
Stanvic <stanvicbest@gmail.com>
2024-06-26 05:44:34 -0700

    trying pos print2

--- app/views/posts/print_pos.php
+++ app/views/posts/print_pos.php
@@ -2,14 +2,11 @@
 <?php
 
 $a = array();
-$obj5->type = 4;
-$obj5->content = "<center><span style=\"font-weight:bold; font-size:20px;\">" .$_SESSION['user_name']. "</span></center>";
-array_push($a,$obj5);
 
 $obj1->type = 0;
-$obj1->content = $_SESSION['user_dsc'] .'<br />'. $_SESSION['address'];	
-$obj1->bold = 0;
-$obj1->align = 1;
+$obj1->content = 'My Title';	
+$obj1->bold = 1;
+$obj1->align =1;
 $obj1->format = 3;
 array_push($a,$obj1);
 
@@ -18,11 +15,16 @@ $obj6->content = ' ';
 $obj6->bold = 0;
 $obj6->align = 0;
 array_push($a,$obj6);
-
+	
 $obj5->type = 4;
-$obj5->content = "<center><span style=\"font-weight:bold; font-size:20px;\">" .$_SESSION['user_phone']. "</span></center>";
-array_push($a,$obj5);	
+$obj5->content = "<center><span style=\"font-weight:bold; font-size:20px;\">This is sample text</span></center>";
+array_push($a,$obj5);
 
+$obj7->type = 0;
+$obj7->content = 'This text has<br />two lines';
+$obj7->bold = 0;
+$obj7->align = 0;
+array_push($a,$obj7);
 
 echo json_encode($a,JSON_FORCE_OBJECT);
 
