b0a4fc0c4666b27d5a47a62af423b86e2ffdb055
Stanvic <stanvicbest@gmail.com>
2024-07-11 18:02:22 +0100

    vendor folder return

--- app/config/config.php
+++ app/config/config.php
@@ -4,12 +4,12 @@ date_default_timezone_set("Africa/Lagos");
   define("DB_HOST", "localhost");
   define("DB_USER", "root");
   define("DB_PASS", "");
-  define("DB_NAME", "emulate");
+  define("DB_NAME", "invoicing");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/invoice');
+  define('URLROOT', 'http://localhost/invoicing');
   // Site Name
   define('SITENAME', 'InvoiceOnline');
   define('SITENAME2', 'Stanvic Concepts | InvoiceOnline');
